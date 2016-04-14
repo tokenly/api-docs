179da1bf3216e46db5c69a6f5949cec4d380fa76
Devon Weller <devon@tokenly.co>
2016-04-14 08:23:41 -0500

    Added lockfile check

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+die() { echo "$@" 1>&2 ; exit 1; }
+
+# checkout
+if [ -e ./source/.lockfile ]; then
+    die "Found lockfile.  Please commit the folder first. "
+fi;
+
 ls sources | xargs -L 1 ./scripts/build-folder.sh
 
 # cp the root files
